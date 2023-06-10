90b3cd646741a670dc178a22b830d83cf0d1d4f8
dario russo <dario.russo.052001@gmail.com>
2023-06-10 21:35:00 +0100

    fix: Link fontsize on Small Size Navbar (Dropdown)

--- components/PortfolioNav/PortfolioNav.tsx
+++ components/PortfolioNav/PortfolioNav.tsx
@@ -71,9 +71,9 @@ export default function NavBar() {
                             href={item.href}
                             className={classNames(
                               item.current
-                                ? "bg-primary-500/70 text-white"
-                                : "text-white hover:bg-primary-500",
-                              "px-5 py-4 rounded-md text-md font-medium"
+                                ? "bg-primary-500/70"
+                                : "hover:bg-primary-500",
+                              "px-5 py-4 rounded-md text-md text-white font-medium"
                             )}
                             aria-current={item.current ? "page" : undefined}
                           >
@@ -105,9 +105,9 @@ export default function NavBar() {
                         key={item.name}
                         className={classNames(
                           item.current
-                            ? "bg-primary-500/70 text-white"
-                            : "text-white hover:bg-primary-500",
-                          "block rounded-md px-4 py-2 text-sm font-medium text-white"
+                            ? "bg-primary-500/70"
+                            : "hover:bg-primary-500",
+                          "block rounded-md px-4 py-2 text-lg font-medium text-white"
                         )}
                         aria-current={item.current ? "page" : undefined}
                       >
