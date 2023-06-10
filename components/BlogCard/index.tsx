77356da4d81bf81bb7b4ebd413d53021ce677cfc
dario russo <dario.russo.052001@gmail.com>
2023-06-10 21:14:26 +0100

    fix: projects page make the entire post card a link

--- components/BlogCard/index.tsx
+++ components/BlogCard/index.tsx
@@ -18,34 +18,34 @@ const BlogCard = ({
   tag,
 }: ComponentProps) => {
   return (
-    <article className="flex max-w-sm flex-col items-start justify-between bg-white rounded-lg">
-      <Image
-        src={imageSrc}
-        alt=""
-        width={2000}
-        height={2000}
-        className="bg-cover mb-3"
-      />
+    <Link href={`projects/${slug}`}>
+      <article className="group flex max-w-sm flex-col items-start justify-between bg-white rounded-t-lg">
+        <Image
+          src={imageSrc}
+          alt=""
+          width={2000}
+          height={2000}
+          className="bg-cover mb-3 rounded-t-lg"
+        />
 
-      <div className="flex items-center gap-x-4 text-xs px-4">
-        <time className="text-gray-500">{date}</time>
-        <span className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
-          {tag}
-        </span>
-      </div>
+        <div className="flex items-center gap-x-4 text-xs px-4">
+          <time className="text-gray-500">{date}</time>
+          <span className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+            {tag}
+          </span>
+        </div>
 
-      <div className="group relative px-4">
-        <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-          <Link href={`projects/${slug}`}>
+        <div className="group relative px-4">
+          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 group-hover:underline">
             <span className="absolute inset-0" />
             {title}
-          </Link>
-        </h3>
-        <p className="my-3 line-clamp-3 text-sm leading-6 text-gray-600">
-          {description}
-        </p>
-      </div>
-    </article>
+          </h3>
+          <p className="my-3 line-clamp-3 text-sm leading-6 text-gray-600">
+            {description}
+          </p>
+        </div>
+      </article>
+    </Link>
   );
 };
 
