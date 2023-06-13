a5785823bf88ced5d4cd34b18c81e1095b1478e6
dario russo <dario.russo.052001@gmail.com>
2023-06-13 03:28:11 +0100

    about page part 2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,11 +10,11 @@ const About = () => {
       <main className="container mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-2 sm:py-6 lg:py-8">
         <section className="mb-8">
           <div className="grid grid-cols-5 grid-rows-3 grid-flow-row gap-2 wrap">
-            <div className="row-span-2 col-span-2 h-full w-full">
+            <div className="row-span-2 col-span-2 bg-pink-500 h-full w-full">
               <Image
                 src="/about/dario-01.jpg"
-                width={512}
-                height={512}
+                width={32}
+                height={32}
                 alt="test image 1"
                 className="h-full w-full object-cover"
                 priority
@@ -43,8 +43,8 @@ const About = () => {
             <div className="row-span-1 col-span-1 bg-slate-500 h-full w-full">
               <Image
                 src="/about/dario-04.jpg"
-                width={32}
-                height={32}
+                width={512}
+                height={512}
                 alt="test image 4"
                 className="object-cover"
                 priority
@@ -93,7 +93,7 @@ const About = () => {
             career, but I&#39;ve had the incredible opportunity to work as a web
             developer at the BBC for a solid two years. This experience has
             truly fueled my enthusiasm for creating websites that users simply
-            can&#39;t resist
+            can&#39;t resist.
           </p>
           <p className="text-lg font-normal text-justify slate-700 mt-4">
             I am an incredibly driven individual who finds immense satisfaction
@@ -123,25 +123,99 @@ const About = () => {
           <h2 className="text-7xl font-extrabold text-slate-900 mt-8">
             Professional Experience
           </h2>
-          <h3 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl mt-4">
-            Trainee Software Engineer
-          </h3>
-          <p className="text-lg font-normal text-justify slate-700 before:content-['-'] before:mr-1 pl-6">
-            BBC World Service Team
-          </p>
-          <p className="text-lg font-normal text-justify slate-700 mt-4">
-            COMING SOON!!!!
-          </p>
+          <div className="bg-white p-4 rounded-lg mt-4">
+            <div className="flex justify-between">
+              <h3 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl">
+                <span>
+                  Trainee Software Engineer
+                  <span className="text-lg font-normal text-justify slate-700 before:content-['-'] before:mr-1 pl-6">
+                    BBC World Service Team
+                  </span>
+                </span>
+              </h3>
+              <span className="block mt-auto">
+                <time>September 2021</time> to <time>June 2023</time>
+              </span>
+            </div>
 
-          <h3 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl mt-4">
-            Teaching Assistant
-          </h3>
-          <p className="text-lg font-normal text-justify slate-700 before:content-['-'] before:mr-1 pl-6">
-            Queen Mary University of London
-          </p>
-          <p className="text-lg font-normal text-justify slate-700 mt-4">
-            COMING SOON!!!!
-          </p>
+            <ul className="space-y-4 text-slate-700 list-disc mt-4">
+              <li className="ml-5">
+                Collaborated closely with product managers, UX designers, and
+                testers to ensure the ongoing maintenance of an accessible
+                front-end for rendering CMS News. This front-end played a vital
+                role in facilitating the successful delivery of 44 news services
+                worldwide.
+              </li>
+              <li className="ml-5">
+                By actively pairing with other 2 junior software engineers, I
+                successfully contributed to reducing the completion time of pull
+                requests (PRs) by 10-20 minutes. Our collaboration focused on
+                migrating the legacy component&#39;s library into the main
+                repository&#39;s component folder, resulting in improved
+                efficiency and streamlined development processes.
+              </li>
+              <li className="ml-5">
+                Took on the responsibility of experimenting with newly created
+                component standards. My task involved refactoring two legacy
+                promo components into a single compound component. This
+                refactoring aimed to provide valuable feedback on the
+                effectiveness of the new standards and demonstrate their
+                benefits to the team. By making the code more reusable and
+                readable, the refactored compound component served as a tangible
+                example of how the new standards could enhance the team&#39;s
+                development process.
+              </li>
+              <li className="ml-5">
+                Through my advocacy and development efforts, I successfully
+                implemented an efficient indexing system for our documentation,
+                which was previously scattered across different locations. This
+                solution involved creating a centralized index portal utilizing
+                storybook, which empowered every team member to swiftly locate
+                and access the pertinent documentation they needed. As a result,
+                we observed a significant boost in engagement with the
+                documentation, coupled with a remarkable reduction in the time
+                required to locate it.
+              </li>
+            </ul>
+          </div>
+
+          <div className="bg-white p-4 rounded-lg mt-4">
+            <div className="flex justify-between">
+              <h3 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl">
+                <span>
+                  Demonstrator
+                  <span className="text-lg font-normal text-justify slate-700 before:content-['-'] before:mr-1 pl-6">
+                    Queen Mary University of London
+                  </span>
+                </span>
+              </h3>
+              <span className="block mt-auto">
+                <time>September 2020</time> to <time>September 2021</time>
+              </span>
+            </div>
+            <ul className="space-y-4 text-slate-700 list-disc mt-4">
+              <li className="ml-5">
+                Providing hands-on guidance and support to first-year students
+                during laboratory sessions for the Computer Systems and Networks
+                and Procedural Programming modules.
+              </li>
+              <li className="ml-5">
+                I was responsible for guiding students on the purpose and
+                approach of the laboratory session, while also providing support
+                and addressing any inquiries they had.
+              </li>
+              <li className="ml-5">
+                I had the responsibility of evaluating and grading students&#39;
+                lab work and coursework, ensuring fair assessment and
+                determining their pass or fail status accordingly.
+              </li>
+              <li className="ml-5">
+                Throughout the session, I effectively managed the simultaneous
+                marking of students&#39; lab requests and questions by resolving
+                20-30 requests per hour.
+              </li>
+            </ul>
+          </div>
         </section>
 
         <section className="mb-8">
